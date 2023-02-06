01ff1eaed929d6696fe0fd028b00041d14c1e560
na0th <105775683+na0th@users.noreply.github.com>
2023-02-06 11:40:29 +0900

    모바일/PC 구별하는 그림판 구현

--- 최건/canvas/app.js
+++ 최건/canvas/app.js
@@ -1,20 +1,20 @@
-// 캔버스 
+// 캔버스
 const canvas = document.getElementById("jsCanvas");
-// 선 
+// 선
 const ctx = canvas.getContext("2d");
-// 색깔 
+// 색깔
 const range = document.getElementById("jsRange");
 
 //컨버스 크기
 canvas.width = 700;
 canvas.height = 700;
 //일단 흰색으로 채우고 시작 (기존 canvas가 흰색으로 보여도 데이터 상으로는 0으로 되어 있다고 한다)
-ctx.fillStyle = "white";  
+ctx.fillStyle = "white";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 
 ctx.strokeStyle = "#2c2c2c"; // 우리가 그릴 선들은 모두 이 색을 갖는다
-ctx.fillStyle = "#2c2c2c"; 
+ctx.fillStyle = "#2c2c2c";
 ctx.lineWidth = 2.5; // 라인의 너비가 2.5
 
 
@@ -27,14 +27,14 @@ function stopPainting(){
 function onMouseMove(event){
     const x = event.offsetX;
     const y = event.offsetY;
-    
+
     if (!painting){
         ctx.beginPath();
         ctx.moveTo(x,y);
     }else {
         ctx.lineTo(x, y); // lineTo는 path의 이전 위치에서 지금 위치까지 선을 만드는 것
         ctx.stroke();
-      } 
+      }
 
 }
 // 마우스를 누르고 있을 때
@@ -52,7 +52,7 @@ function onMouseLeave(event){
 // canvas 그림 리셋
 document.getElementById("jsReset").onclick=function(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "white";  
+    ctx.fillStyle = "white";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 // canvas 그림 저장
@@ -66,10 +66,10 @@ function handleSaveClick() {
     link.href = image;
     link.download = "PaintJS";
     link.click();
-  }  
+  }
 if (jsSave) {
     jsSave.addEventListener("click", handleSaveClick);
-  }  
+  }
 //캔버스 내 이벤트들
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
@@ -85,8 +85,9 @@ if(canvas){
 if (range) {
     range.addEventListener("input", handleRangeChange);
 }
-
 function handleRangeChange(event) {
   const size = event.target.value;
   ctx.lineWidth = size;
 }
+
+
