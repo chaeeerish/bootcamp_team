cf177345b044af54ef223e3faaac025c10075f2f
ChaeRin Yang <keukkeu464@gmail.com>
2023-02-16 19:03:31 +0900

    jwt 실패..

--- 양채린/backend/app.py
+++ 양채린/backend/app.py
@@ -11,15 +11,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:1234@127.0.0.1:330
 app.config['SQLARCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'ekdwls'
 
-# 토큰 생성에 사용될 Secret Key를 flask 환경 변수에 등록
-app.config.update(
-			DEBUG = True,
-			JWT_SECRET_KEY = "I'M IML"
-		)
-
-# JWT 확장 모듈을 flask 어플리케이션에 등록
-jwt = JWTManager(app)
-
 db.init_app(app)
 
 # 블루프린트
